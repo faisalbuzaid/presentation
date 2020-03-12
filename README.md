429217e5dc69ca7a5c8c3837fe8573dfe1f25c83
fbuzaid <f.buzaid@umail.ucc.ie>
2020-03-12 10:02:17 +0300

    project

---
+++ README.md
@@ -0,0 +1,8 @@
+Part of this work was contributed from https://dev.to/bornasepic/pure-and-simple-tic-tac-toe-with-javascript-4pgn and https://www.codesdope.com/blog/article/12-creative-css-and-javascript-text-typing-animati/
+
+The main technologies were used: 
+- HTML
+- CSS
+- javaScript 
+
+By mimicking an existing tic tac toe game, we observed some of the js functionality and we understand how we can make the client-side active by joining the HTML/CSS by the js.   
---
+++ main.js
@@ -0,0 +1,153 @@
+const currentStatusDisplay = document.querySelector('.game__status');
+
+var isGameActive = true;
+var currentPlayer = "O";
+var holdGameState = ["", "", "", "", "", "", "", "", ""];
+
+const winnerMessage = () => `Congratulations ${currentPlayer} , you are the champion`;
+const noWinner = () => `No winner!!! try again!!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+
+currentStatusDisplay.innerHTML = currentPlayerTurn();
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    holdGameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
+
+function handlePlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    currentStatusDisplay.innerHTML = currentPlayerTurn();
+}
+
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        let a = holdGameState[winCondition[0]];
+        let b = holdGameState[winCondition[1]];
+        let c = holdGameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break
+        }
+    }
+
+    if (roundWon) {
+        currentStatusDisplay.innerHTML = winnerMessage();
+        isGameActive = false;
+        return;
+    }
+
+    let roundDraw = !holdGameState.includes("");
+    if (roundDraw) {
+        currentStatusDisplay.innerHTML = noWinner();
+        isGameActive = false;
+        return;
+    }
+
+    handlePlayerChange();
+}
+
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+
+    if (holdGameState[clickedCellIndex] !== "" || !isGameActive) {
+        return;
+    }
+
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
+}
+
+function handleRestartGame() {
+    isGameActive = true;
+    currentPlayer = "X";
+    holdGameState = ["", "", "", "", "", "", "", "", ""];
+    currentStatusDisplay.innerHTML = currentPlayerTurn();
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
+}
+
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('.restart__game').addEventListener('click', handleRestartGame);
+
+
+
+
+
+
+
+
+//for the header <h1> rotating 
+
+var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+  
+  TxtRotate.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+  
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+  
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+  
+    if (this.isDeleting) { delta /= 2; }
+  
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+  
+    setTimeout(function() {
+      that.tick();
+    }, delta);
+  };
+  
+  window.onload = function() {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-rotate');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css);
+  };
